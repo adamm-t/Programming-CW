3eca2c0ed7fc1aad3c2a47d62a078b58f4dbb36c
Adam <adamayad@outlook.com>
2025-11-19 20:27:28 +0200

    Update Steganography.py

--- Steganography.py
+++ Steganography.py
@@ -28,7 +28,7 @@ def encode_message():
     print("\n--- ENCODING ---")
     bmp_name = input("Enter BMP file name to encode into: ")
 
-    #now we attempt to read the BMP file and make sure a message is entered for the encoding process to work
+#now we attempt to read the BMP file and make sure a message is entered for the encoding process to work
     try:
         with open(bmp_name, "rb") as f:
             bmp_bytes = bytearray(f.read())
@@ -41,8 +41,23 @@ def encode_message():
         print("Error: please input a message to hide")
         return
     
-    #here we convert the message into binary using the function we made and store it into a variable
+#here we convert the message into binary using the function we made and store it into a variable
     message_bits = text_to_binary(message)
+
+#check if message will fit inside the image by comparing number of bits needed for message and number of bits available in image after subtracting header pixels
+    if len(message_bits) > len(bmp_bytes) - header_size:
+        print("Error: message is too long for this image.")
+        return
     
+    pixel_index = header_size
+
+#now we replace the LSB of each byte with the bits from our message using bitwise operations AND to clear the LSB and then OR to write the LSB
+    for bit in message_bits:
+        bmp_bytes[pixel_index] &= 0b11111110
+        bmp_bytes[pixel_index] |= int(bit)
+        pixel_index += 1
+    
+
+
 
 
